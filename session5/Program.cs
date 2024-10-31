14de7867c64dfae1b495054e95e42154f8571b7a
cuongmui1984 <sagittariusvndn2014@gmail.com>
2024-10-31 08:50:41 +0700

    Push code session5

---
+++ session5/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // gọi hàm
+        int ketQua = tinhTong(10,11);
+        Console.WriteLine($"tổng 2 số là: {ketQua}");
+        Console.WriteLine("Bài 1: nhập vào số n từ bàn phím. In ra các số nguyên tố từ 2 đến n");
+Console.WriteLine("Mời bạn nhập số: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int count = 2; count <= number; count++){
+    bool checkPrime = isPrime(count);
+    
+
+    if(checkPrime == true) {
+        Console.Write($"{count} ");
+    }
+}
+
+        
+    }
+    // khai báo hàm kiểm tra số nguyên tố
+    private static bool isPrime(int number ){
+        if (number<2) {
+            return false;
+        }
+        for (int count = 2; count<= Math.Sqrt(number); count++){
+            if (number%count==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    // khai báo hàm
+    private static int tinhTong(int number1, int number2){
+        return number1 + number2;
+    }
+}
