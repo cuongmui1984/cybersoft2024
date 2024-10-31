6b619230d8255f882123fe689859cfe237b857e6
cuongmui1984 <sagittariusvndn2014@gmail.com>
2024-10-31 08:55:11 +0700

    push code tuhoc

---
+++ tuhoc/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float x, y;
+        x = 5;
+        y = 4;
+        System.Console.WriteLine("vay gia tri cua x = {0} va y = {1}" , x, y);
+        
+        x+=y; // x = x +y
+        System.Console.WriteLine("x = " + x);
+        x-=y; // x = x -y
+        System.Console.WriteLine("x = " + x);
+        
+
+
+    }
+}
